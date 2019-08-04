876cf568fb1ceef84a751c0a89cdb76f39dc678d
sharanr91 <sharanr91@Dear.General.github.com>
2019-08-04 14:22:21 +1000

    update our menu items to navigate with Router HOC

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  //we will wrap up our application with "browserRouter"
+  //this component gives our applicaiton sitting between the component
+  //all the functionality of routing, that this library provides
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
