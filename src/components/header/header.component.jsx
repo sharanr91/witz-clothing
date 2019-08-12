cf35816db1c0d232153d98c0b120f052e2275920
sharanr91 <sharanr91@Dear.General.github.com>
2019-08-12 21:20:54 +1000

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import "./header.styles.scss";
 import { ReactComponent as Logo, ReactComponent } from "../../assets/crown.svg";
+import { auth } from "./../../firebase/firebase.utils";
 
-const Header = () => {
+const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -17,6 +17,15 @@ const Header = () => {
         <Link className="option" to="/contact">
           CONTACT
         </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
       </div>
     </div>
   );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyDDFn9NnVNWJ8PxOFl4JqJPozxO16BY_e4",
+  authDomain: "witz-db.firebaseapp.com",
+  databaseURL: "https://witz-db.firebaseio.com",
+  projectId: "witz-db",
+  storageBucket: "",
+  messagingSenderId: "204911789921",
+  appId: "1:204911789921:web:a03f04c68bb3a328"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
